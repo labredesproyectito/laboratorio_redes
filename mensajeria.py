dcdee7c6436c93ebc34cdb35c1f2898f1f290240
rafael padron <rafael1632006@gmail.com>
2026-06-04 00:12:14 -0300

    prueba 2

--- mensajeria.py
+++ mensajeria.py
@@ -1,11 +1,11 @@
-import socket
-import threading
-import hashlib
-import os
-import sys
-from datetime import datetime
 import argparse
 import base64
+import hashlib
+import os
+import socket
+import sys
+import threading
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("puerto", type=int)
@@ -19,7 +19,6 @@ MAX_LARGO_MENSAJE = 255
 def autenticador():
 
     while True:
-
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         sock.connect((args.ipAuth, args.portAuth))
@@ -55,16 +54,11 @@ def parsear(texto):
 
     ip = partes[0]
 
-    if len(partes) < 2:
-        return ip, None, None
-
     if partes[1].startswith("&"):
-
         menj = partes[1]
         path = partes[2] if len(partes) > 2 else None
 
     else:
-
         menj = " ".join(partes[1:])
         path = None
 
@@ -76,6 +70,7 @@ def conectar_tcp(ip, puerto):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        print(f"Conectando con {(ip, puerto)}")
         sock.connect((ip, puerto))
         return sock
 
@@ -90,12 +85,12 @@ def envio(mensaje, ip):
     datos = mensaje.encode("utf-8")
 
     if ip == "*":
-
+        print("Envio UDP")
         broad_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         broad_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     else:
-
+        print("Envio TCP")
         sock = conectar_tcp(ip, args.puerto)
 
         if not sock:
@@ -104,11 +99,10 @@ def envio(mensaje, ip):
     i = 0
 
     while i < len(datos):
-
-        bloque = datos[i:i + 255]
+        bloque = datos[i : i + 255]
 
         if ip == "*":
-            broad_sock.sendto(bloque,("255.255.255.255", args.puerto))
+            broad_sock.sendto(bloque, ("255.255.255.255", args.puerto))
         else:
             sock.sendall(bloque)
 
@@ -128,12 +122,15 @@ def enviar_mensaje(nombre):
 
     (ip, menj, path) = parsear(mensaje)
 
+    print(f"IP: {ip}")
+    print(f"Mensaje: {menj}")
+    print(f"Path: {path}")
+
     if path is not None:
-        
+        print("Enviar archivo")
         with open(path, "r", encoding="utf-8") as f:
             path = f.read()
-            nombre_archivo = os.path.basename(nombre_archivo)
-            path = base64.b64encode(path).decode("utf-8")
+            nombre_archivo = os.path.basename(path)
             menj = menj + nombre_archivo + " " + path
 
     if menj is None:
@@ -142,8 +139,6 @@ def enviar_mensaje(nombre):
     menj = ip + " " + nombre + " " + menj + "\r\n"
 
     envio(menj, ip)
-
-
 
 
 def manejar(data_bytes: bytes):
@@ -165,39 +160,21 @@ def manejar(data_bytes: bytes):
     automatico = fecha + " " + ip + " "
 
     if resto.startswith("&file "):
-
         try:
-             nombre_archivo, b64_data = resto.split(" ", 2)
+            nombre_archivo, archivo_bytes = resto.split(" ", 2)
         except ValueError:
             print(automatico + "<Error Recibiendo Archivo de " + nombre)
             return False
 
-        
-
-        try:
-            archivo_bytes = base64.b64decode(b64_data)
-        except Exception as e:
-            print(automatico + "<Error Recibiendo Archivo de " + nombre)
-            print(e)
-            return False
-
-        
-
+        archivo_bytes = archivo_bytes.encode()
         with open(nombre_archivo, "wb") as f:
             f.write(archivo_bytes)
 
-        print(
-            automatico +
-            "Recibiendo " +
-            nombre_archivo +
-            " de " +
-            nombre
-        )
+        print(automatico + "Recibiendo " + nombre_archivo + " de " + nombre)
 
         return True
 
     else:
-
         mensaje = resto
 
         nombre_log = f"{ip}.txt"
@@ -208,10 +185,11 @@ def manejar(data_bytes: bytes):
         print(automatico + nombre + " dice: " + mensaje)
 
         return True
+
+
 def handler(data_bytes):
     print("RECIBIDO:", data_bytes)
     manejar(data_bytes)
-
 
 
 def broadcast_udp(host, port, handler):
@@ -221,8 +199,12 @@ def broadcast_udp(host, port, handler):
     while True:
         data = s.recv(255)
         handler(data)
+
+
 def escucha_tcp(host, port, handler):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"Host tcp server socker:{host}")
+    print(f"Port tcp server socker:{port}")
     s.bind((host, port))
     s.listen()
 
@@ -231,27 +213,25 @@ def escucha_tcp(host, port, handler):
         conn, addr = s.accept()
         print(f"Cliente conectado desde {addr}")
 
-
         threading.Thread(
-            target=manejar_cliente,args=(conn, handler),daemon=True).start()
+            target=manejar_cliente, args=(conn, handler), daemon=True
+        ).start()
+
 
 def manejar_cliente(conn, handler):
     buffer = b""
     print("reciviendo mensaje")
 
     DELIM = b"\r\n"
-    
+
     while True:
         try:
             chunk = conn.recv(255)
             if not chunk:
-                print("no es un chunk, rompiendo")
                 break
 
             buffer += chunk
-            
 
-           
             while DELIM in buffer:
                 mensaje, buffer = buffer.split(DELIM, 1)
                 print("mensaje recibido... procesando")
@@ -262,31 +242,31 @@ def manejar_cliente(conn, handler):
             print("conexion fallo")
             print(e)
             break
-    
+
     conn.close()
 
 
 def iniciar_servidor(host, tcp_port, udp_port, handler):
     threading.Thread(
-        target=escucha_tcp,args=(host, tcp_port, handler),daemon=True).start()
+        target=escucha_tcp, args=(host, tcp_port, handler), daemon=True
+    ).start()
 
     threading.Thread(
-        target=broadcast_udp,args=(host, udp_port, handler),daemon=True).start()
+        target=broadcast_udp, args=(host, udp_port, handler), daemon=True
+    ).start()
+
 
 def main():
 
     nombre = autenticador()
+    # ToDo
     print(f"Usuario autenticado: {nombre}")
     host = "0.0.0.0"
 
-   
-
-    threading.Thread(
-        target=iniciar_servidor,args=(host, args.puerto, args.puerto, manejar),daemon=True).start()
+    iniciar_servidor(host, args.puerto, args.puerto, manejar)
 
     print("Servidor de escucha iniciado")
 
-   
     while True:
         enviar_mensaje(nombre)
 
